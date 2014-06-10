3e021c3c3a50291dfa2b2e113ca10e023b508113
Ismail Akram <rubyonrails3@gmail.com>
2014-06-10 16:37:57 +0500

    added staging settings

--- config/deploy.rb
+++ config/deploy.rb
@@ -19,7 +19,7 @@ set :deploy_to, "/home/#{fetch(:user)}/#{fetch(:application)}"
 # set :format, :pretty
 
 # Default value for :log_level is :debug
-# set :log_level, :debug
+set :log_level, :info
 
 # Default value for :pty is false
 # set :pty, true
